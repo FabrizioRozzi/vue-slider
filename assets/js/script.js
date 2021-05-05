17d65cb3e3449c604522ca020c3468720f0f3c73
FabrizioRozzi <rozzi458@gmail.com>
2021-05-05 16:22:55 +0200

    html css finito

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const app = new Vue({
+
+  el:'#app',
+  data : {
+
+    arrImage = [
+      'assets/img/image1',
+      'assets/img/image2',
+      'assets/img/image3',
+      'assets/img/image4'
+     
+    ]
+
+  }
+
+});
